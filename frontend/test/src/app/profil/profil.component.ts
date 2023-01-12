20d3e310f0f53eaa7f18380c1711237bcfba37fd
joumarhiba <joumhiba7@gmail.com>
2023-01-13 00:14:02 +0100

    download image part 1

--- frontend/test/src/app/profil/profil.component.ts
+++ frontend/test/src/app/profil/profil.component.ts
@@ -12,11 +12,26 @@ export class ProfilComponent {
 
   constructor(private profilService: ProfilService, private snackBar: MatSnackBar){}
 
+  public imgURL: any
+  public imagePath : any;
+  public userFile: any;
   public file: File;
 
   onFilechange(event: any) {
     console.log(event.target.files[0])
     this.file = event.target.files[0]
+    var mimeType = event.target.files[0].type
+    if(mimeType.match(/image\/*/) == null){
+        console.log("only images are supported");
+        return;
+    }
+
+    var reader = new FileReader();
+        this.imagePath = this.file;
+        reader.readAsDataURL(this.file)
+        reader.onload = (_event) => {
+          this.imgURL = reader.result
+        }
   }
 
   openSnackBar(message: string) {
--- frontend/test/src/app/sidebar/sidebar.component.ts
+++ frontend/test/src/app/sidebar/sidebar.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  constructor(private httpClient: HttpClient){}
+
+  retrievedImage: any;
+  base64Data: any;
+  retrieveResonse: any;
+
+  ngOnInit(): void {
+
+  }
+
+
 
 }
